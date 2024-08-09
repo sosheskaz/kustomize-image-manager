258821d7fbde49ea4b12d1f3e7a63062c4259415
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2024-08-08 22:01:20 -0500

    skip_point false for tailscale

--- auto-update.sh
+++ auto-update.sh
@@ -13,7 +13,8 @@ do
   repo="$(IMAGE_NAME="$mi" yq e '.managed_images[env(IMAGE_NAME)].repo // env("IMAGE_NAME")' -r config.yaml)"
   matcher="$(IMAGE_NAME="$mi" yq e '.managed_images[env(IMAGE_NAME)].matcher // env("IMAGE_NAME")' -r config.yaml)"
   sorter="$(IMAGE_NAME="$mi" yq e '.managed_images[env(IMAGE_NAME)].sorter // "version"' -r config.yaml)"
-  latest="$(SORTER="$sorter" ./get-latest-image.sh "$repo" "$matcher")"
+  skip_point="$(IMAGE_NAME="$mi" yq e '.managed_images[env(IMAGE_NAME)].skip_point | (. == null or . == true)' -r config.yaml)"
+  latest="$(SORTER="$sorter" SKIP_POINT_RELEASE="$skip_point" ./get-latest-image.sh "$repo" "$matcher")"
   echo "setting $mi to $repo:$latest" >&2
   IMAGE_NAME="$mi" REPO="$repo" TAG="$latest" yq e -P -i 'with(.images[env(IMAGE_NAME)]; .newName = env(REPO) | .newTag = (env(TAG) | to_string))' 'versions.yaml'
 done
